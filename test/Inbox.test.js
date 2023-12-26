97855ae737ab92a2c754f666d32f4e293c5fa4be
Piotr Podgórski <p.t.podgorski+git@gmail.com>
2023-12-26 16:23:14 +0100

    inbox contract tested succesfully

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,10 +1,11 @@
 import assert from "assert";
-import ganache from "ganache-cli";
-import { beforeEach } from "mocha";
-import { Web3 } from "web3";
+import ganache from "ganache";
+import { beforeEach, it } from "mocha";
+import { Web3, net } from "web3";
 import { abiInterface, bytecode } from "../compile.js";
 
 const web3 = new Web3(ganache.provider());
+const INITIAL_MESSAGE = "Siemanko!";
 
 let accounts;
 let inbox;
@@ -15,12 +16,24 @@ beforeEach(async () => {
 
   //use one of those accounts to deploy
   inbox = await new web3.eth.Contract(JSON.parse(abiInterface))
-    .deploy({ data: bytecode, arguments: ["siemanko!"] })
+    .deploy({ data: bytecode, arguments: [INITIAL_MESSAGE] })
     .send({ from: accounts[0], gas: "1000000" });
 });
 
 describe("Inbox", () => {
   it("deploys a contract", () => {
-    console.log(inbox);
+    assert.ok(inbox.options.address);
+  });
+
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, INITIAL_MESSAGE);
+  });
+
+  it("can chabge the message", async () => {
+    const newMessage = "zmieniony message";
+    await inbox.methods.setMessage(newMessage).send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, newMessage);
   });
 });
